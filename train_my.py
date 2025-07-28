def390a90db00451252f36452da57f4bcac56d0a
ohyeon <kosh2040@kaist.ac.kr>
2025-07-28 22:11:42 +0900

    feat : ppo2

--- train_my.py
+++ train_my.py
@@ -10,26 +10,30 @@ if __name__ == '__main__':
     env_name = "Ant-v5"
 
     # num_envs = 1
-    num_envs = 1024
+    num_envs = 12
     horizon_length = 16
     batch_size = num_envs * horizon_length
-    num_epochs = 10000
+    num_epochs = 5
+    algorithm = 'ppo'
+    num_mini_epochs = 4
 
-    iswandb = True
+    iswandb = False
 
 
     istrain = True
     isload = False
-    loaddir = 'Ant-v5_10_20250726_000450_Norm obs Denorm Val'
-    loaddir = 'Ant-v5_32_20250726_000942_Norm obs Denorm Val'
+    loaddir = 'Ant-v5_10_20250725_193336_Norm obs Denorm Val'
+    # loaddir = 'Ant-v5_32_20250726_000942_Norm obs Denorm Val'
+    # loaddir = 'Ant-v5_32_20250728_191815_PPO Mini Epoch + Running'
+    # loaddir = 'Ant-v5_1024_gae'
     # loaddir = 'Ant-v5_1024_nogae'
     # loaddir = 'Ant-v5_old'
-    loadpoint = 9999
+    loadpoint = 9500
     loadpath = f"run/{loaddir}/{str(loadpoint)}.pth"
 
 
     
-    save_name = "Norm obs Denorm Val"
+    save_name = "PPO Mini Epoch + Running"
 
 
     # Gymnasium Environment
@@ -59,7 +63,7 @@ if __name__ == '__main__':
 
 
     # Load
-    agent =  PolicyGradeintAgent(envs, num_envs, num_epochs, horizon_length, batch_size, save_name, iswandb)
+    agent =  PolicyGradeintAgent(envs, num_envs, num_epochs, horizon_length, batch_size, save_name, algorithm, num_mini_epochs, iswandb)
     if isload:
         agent.load(loadpath)
 
